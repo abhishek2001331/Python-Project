acb2696388278f869dbaf3abc3e8b1809e9978fe
Abhishek Dubey <abhishekbrijeshdubey@gmail.com>
2026-09-15 10:40:25 +0530

    pythonModule

---
+++ PPTABHISHEKLIST/trening.py/pythonModule/mymain.py
@@ -0,0 +1,15 @@
+import calculator
+print(calculator.add(10,20))
+print(calculator.sub(10,20))
+print(calculator.mul(10,20))
+print(calculator.div(10,20))
+
+
+import math
+
+print(math.sqrt(25))
+
+from math import sqrt, factorial
+
+print(sqrt(25))
+print(factorial(5))
